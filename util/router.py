2e5a7b3c4c75e2c90ac0b3581749b0f4f6a09804
Zac Sanger <58315356+Zsanger34@users.noreply.github.com>
2024-09-17 18:44:16 -0400

    Add_Route Create and Starting Testing

--- util/router.py
+++ util/router.py
@@ -1,10 +1,24 @@
 class Router:
 
     def __init__(self):
-        pass
+        #Creating a routes Dict
+        self.routes = []
 
+    #This Method is used to specify how to route a specific request based on tis method and path
     def add_route(self, method, path, action, exact_path=False):
-        pass
+        self.routes.append({"method": method,"path": path, "action":action, "exact_path": exact_path})
 
+    #This Method takes a request which has the following
+    # self.body = b""
+    # self.method = ""
+    # self.path = ""
+    # self.http_version = ""
+    # self.headers = {
+    #And matches it to the routes 
     def route_request(self, request, handler):
         pass
+
+def test1():
+    router = Router()
+    router.add_route("Get", "/", route_request, False)
+    assert route.routes[0]["method"] == "Gete"
