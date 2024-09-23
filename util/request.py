455ca1868310e0cf4a66221ec7a83231eb09fa4d
Zac Sanger <sangerzachary@gmail.com>
2024-09-23 14:02:20 -0400

    Attempted to Change how Cookies are parsed Stop Splitting the Body before encode
    All Test are still failing on autolab

--- util/request.py
+++ util/request.py
@@ -16,17 +16,25 @@ class Request:
             return
         #Moving request to a string so i can use .split
         request = request.decode('utf-8')
+        #print(f"Start\nThis is my request: {request}")
         #Seperating the Request by the /r/n/r/n will give me the Headers and Request Line and the Body
-         
-        parsed_header, parsed_body = request.split("\r\n\r\n")
+        if "\r\n\r\n" in request:
+
+            parsed_header, parsed_body = request.split("\r\n\r\n")
+            #print(f" This is my parsed_header {parsed_header}")
+            #print(f" This is my parsed_body\n {parsed_body} \nEnd\n")
+        else:
+            parsed_header, parsed_body = request, ""
         #Seperating Headers and Request Line
         parsed_reqline, parsed_headers = parsed_header.split("\r\n", 1) 
         #Parse the Request Line
-            
+        #print(f"This is my parsed_reqlin {parsed_reqline}")
+        #print(f"This is my parsed_headers {parsed_headers}\n")
         self.method, self.path, self.http_version = parsed_reqline.split()
         #Parse the Headers 
             
         headers_parsed = parsed_headers.split('\r\n')
+        #print(f"This is my headers {headers_parsed}\n")
         #Had an issue with where you need to add the values into the dict can just set the output of split to it 
         for pair in headers_parsed:
             key, value = pair.split(":", 1)
@@ -39,8 +47,10 @@ class Request:
                     key, value = pair.split("=")
                     #print(f"This is the key {key} and this is the value {value}")
                     self.cookies[key.strip()] = value.strip()
+        #print(f"This is my headers {self.headers}\n")
+        #print(f"This is my Cookies {self.cookies}\nEnd\n")
         #Converting Body back to Byte
-        parsed_body = parsed_body.strip().encode('utf-8')
+        parsed_body = parsed_body.encode('utf-8')
         self.body = parsed_body
         
        
@@ -57,6 +67,17 @@ def test1():
     # This is the start of a simple way (ie. no external libraries) to test your code.
     # It's recommended that you complete this test and add others, including at least one
     # test using a POST request. Also, ensure that the types of all values are correct
+def test_withCookies():
+    request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nCookie: id=X6kAwpgW29M; visits=4\r\n\r\n')
+    assert request.method == "GET"
+    assert "Host" in request.headers
+    assert request.headers["Host"] == "localhost:8080"  # note: The leading space in the header value must be removed
+    assert request.body == b""
+    assert 'id' in request.cookies
+    assert request.cookies['id'] == 'X6kAwpgW29M'
+    assert 'visits' in request.cookies
+    assert request.cookies['visits'] == '4'
+
 def test_post_request():
     request = Request(b'POST /path HTTP/1.1\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\nhello')
     assert request.method == "POST"
@@ -85,9 +106,9 @@ def test_incorrect_request():
 
 if __name__ == '__main__':
     test1()
+    test_withCookies()
     test_post_request()
     test_headers_with_spaces()
-    # test_empty_request()
     test_empty_body_POST()
     test_incorrect_request()
     
